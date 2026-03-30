108e39aae49eead991771080b477e7c89af908aa
cjoaquin <joaquin.cozette@ynov.com>
2026-03-30 16:00:26 +0200

    ajout points carte

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -13,16 +13,34 @@ const Map = () => {
       minZoom: 3,
       maxZoom: 19,
       maxBounds: [[-85, -180], [85, 180]],
-    }).setView([48.8566, 2.3522], 13)
+    }).setView([46.6033, 1.8883], 6)
     
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '© OpenStreetMap contributors',
       maxZoom: 19,
       noWrap: true,
     }).addTo(map.current)
+
+    fetch("https://object.files.data.gouv.fr/meteofrance/data/synchro_ftp/OBS/SYNOP/postes_synop.geojson")
+      .then(res => res.json())
+      .then(data => {
+        data.features.forEach(station => {
+          const [lng, lat] = station.geometry.coordinates
+          const { Nom, Altitude, Id } = station.properties
+
+          L.marker([lat, lng])
+            .addTo(map.current)
+            .bindPopup(`
+              <b>Station : ${Nom}</b><br>
+              ID : ${Id}<br>
+              Altitude : ${Altitude}m
+            `)
+        })
+      })
+      .catch(err => console.error("Erreur de chargement des données :", err))
   }, [])
 
-  return <div ref={mapContainer} style={{ width: '100%', height: '500px' }} />
+  return <div ref={mapContainer} style={{ width: '100%', height: '100vh' }} />
 }
 
 export default Map
