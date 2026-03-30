91b1318bb5c3496c13764dd15a8081b003bd3a9d
imanhamdy <iman.hamdy@ynov.com>
2026-03-30 14:19:13 +0000

    infra: remove hardcoded passwords from patroni and postgres config

--- infra/postgres/init.sql
+++ infra/postgres/init.sql
@@ -1,1 +1,4 @@
-CREATE USER replicator WITH REPLICATION PASSWORD 'postgres';
+-- Replication slot for replica
+SELECT pg_create_physical_replication_slot('replication_slot') WHERE NOT EXISTS (
+  SELECT 1 FROM pg_replication_slots WHERE slot_name = 'replication_slot'
+);
